581e3d9ff8072c5b531566e316b812f6a0f52084
MigoDelMundo <migo.delmundo@hotmail.com>
2023-06-03 16:11:42 +0800

    Design bug fixes and changes, redesigned games tab and message composer, along with many others

--- src/components/subpages/home/games.tsx
+++ src/components/subpages/home/games.tsx
@@ -6,6 +6,14 @@ import GameOptionData from "./gameoptiondata";
 const Games = () => {
   return (
     <>
+      <div className="Games-Header">
+        <h1>🕹️ Games 🕹️</h1>
+        <p>
+          Play the latest games by clicking 'Play' and scroll down to see their
+          descriptions! (All games listed in here are all fictional and are
+          unplayable as of the time being. Also, descriptions are scrollable!)
+        </p>
+      </div>
       <div className="Games-Grid">
         {GameOptionData.map((item, index) => (
           <GameOption
