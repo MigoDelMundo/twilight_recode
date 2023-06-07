6e85424f7a6e6b112b9b5f618ba9a0b684aa0d01
MigoDelMundo <migo.delmundo@hotmail.com>
2023-06-07 11:31:50 +0800

    Light mode

--- src/components/subpages/credits/credits.tsx
+++ src/components/subpages/credits/credits.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ToggleModeDiv from "./togglemodediv";
 
 const Credits = () => {
   return (
@@ -10,15 +11,19 @@ const Credits = () => {
           to program with React.js with his course. Highly recommend.
           <br /> Credits to ChatGPT for helping me with so much of the logic and
           errors that came up with the programming of this website. Helped me so
-          much that it's insane.
+          much that it's insane. Also for the feed posts a majority of them came
+          from ChatGPT with my ideas as a base
+          <br /> Credits to Pexels as well because some pictures are owned by
+          them
           <br /> Credits to you as well for reading this part of the website
           haha thanks
           <br />
           <br /> I don't know what else to put so I want to shoutout some other
           people. <br />
-          Shoutout to my dearest mom who bought me a new laptop which was
-          basically where I spent a majority of my coding time in.
+          <br />
+          <br /> Oh, and also,
         </p>
+        <ToggleModeDiv />
       </div>
     </>
   );
